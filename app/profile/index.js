199b6c9ac14e1a87626cba6ed8711fe5eadfdde4
EnzoCasalini <enzocasa83@gmail.com>
2024-05-09 15:14:56 +0200

    Added modification of display name and profile picture in profile

--- app/profile/index.js
+++ app/profile/index.js
@@ -1,5 +1,4 @@
-import "../../firebase/firebaseConfig";
-import {Text, TextInput, View, Image, Button, Pressable} from 'react-native';
+import {Alert, Button, Image, Pressable, Text, TextInput, View} from 'react-native';
 import { getAuth, updateProfile } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { Link } from "expo-router";
@@ -15,86 +14,66 @@ const Profile = () => {
   const [user, setUser] = useState(null);
   const [displayName, setDisplayName] = useState('');
   const [image, setImage] = useState(null);
-  const [hasName, setHasName] = useState(false);
-  const [hasPhoto, setHasPhoto] = useState(false);
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         setUser(user);
-        if (user.displayName) {
-          setDisplayName(user.displayName);
-          setHasName(true);
-        }
-        if (user.photoURL) {
-          setImage(user.photoURL);
-          setHasPhoto(true);
-        }
+        setDisplayName(user.displayName || '');
+        setImage(user.photoURL || '');
       } else {
         setUser(null);
       }
     });
+    return () => unsubscribe(); // Cleanup subscription
   }, []);
 
-  const onLogout = () => {
-    auth.signOut().then(() => {
-      setUser(null);
-    });
-  }
 
   const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
+    const permissionToAccessGallery = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionToAccessGallery.granted === false) {
+      Alert.alert("Permission Required", "Permission to access camera roll is required !");
+      return;
     }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.canceled === true) {
+      return;
+    }
+
+    const image = pickerResult.assets[0].uri;
+    setImage(image);
+    await updateUserPicture(image);
   };
 
-
-  const updateUserName = (displayName) => {
-    verifyDisplayName(displayName);
-
-    if (displayName) {
-      updateProfile(auth.currentUser,{
-        displayName: displayName,
-      }).then(() => {
-        setHasName(true);
-      }).catch((error) => {
-        console.error(error);
-      });
-    }
-  }
-
-  const updateUserPicture = (photoURL) => {
-    if (photoURL) {
-      uploadToFirebase(photoURL).then((url) => {
-        updatePhotoURL(url).then(() => {
-          setHasPhoto(true);
-        });
-      });
-    }
-  }
-
-  const verifyDisplayName = (displayName) => {
-    if (!displayName) {
-      console.log('Please enter a display name');
+  const updateUserName = () => {
+    if (displayName.length < 3 || displayName.length > 20) {
+      console.log('Display name must be between 3 and 20 characters');
       return;
     }
 
-    if (displayName.length < 3) {
-      console.log('Display name must be at least 3 characters');
-      return;
+    updateProfile(auth.currentUser, {displayName}).then(() => {
+      console.log('Display name updated successfully');
+    }).catch((error) => {
+      console.error(error);
+      console.log('Failed to update display name');
+    });
+  };
+
+  const updateUserPicture = async (uri) => {
+    let imageUrl = '';
+    if (uri) {
+      imageUrl = await uploadToFirebase(uri);
     }
 
-    if (displayName.length > 20) {
-      console.log('Display name must be less than 20 characters');
-    }
-  }
+    updatePhotoURL(imageUrl).then(() => {
+      setImage(imageUrl);
+      console.log('Profile picture updated successfully');
+    }).catch((error) => {
+      console.error(error);
+      console.log('Failed to update profile picture');
+    });
+  };
 
   return (
     <LinearGradient colors={['#2a2a2a', '#7a7a7a']} style={styles.container2}>
@@ -103,54 +82,18 @@ const Profile = () => {
           <Text style={styles.profileTitle}>Profile</Text>
           <Text style={styles.text}>Email: {user.email}</Text>
           <Text style={styles.text}>UID: {user.uid}</Text>
-          {user.emailVerified ? (
-            <Text style={styles.text}>Your email is verified :)</Text>
-          ) : (
-            <Text style={styles.text}>Your email is not verified yet :(</Text>
-          )}
-          {hasName && hasPhoto ? (
-            <View>
-              <Image source={{ uri: image }} style={styles.image} />
-              <Text style={styles.text}>Display Name: {displayName}</Text>
-            </View>
-          ) : hasName ? (
-            <View style={styles.form}>
-              <Image source={{ uri: image }} style={styles.image} />
-              <Button title="Pick a different image" onPress={pickImage} />
-              <Pressable onPress={() => updateUserPicture(image)} style={styles.button}>
-                <Text style={styles.text}>Update Picture</Text>
-              </Pressable>
-            </View>
-          ) : hasPhoto ? (
-            <View style={styles.form}>
-              <Text style={styles.label}>Display Name</Text>
-              <TextInput
-                value={displayName}
-                onChangeText={setDisplayName}
-                style={styles.input}
-              />
-              <Pressable onPress={() => updateUserName(displayName)} style={styles.button}>
-                <Text style={styles.text}>Update DisplayName</Text>
-              </Pressable>
-            </View>
-          ) : (
-            <View style={styles.form}>
-              <Text style={styles.label}>Display Name</Text>
-              <TextInput
-                value={displayName}
-                onChangeText={setDisplayName}
-                style={styles.input}
-              />
-              <Pressable onPress={() => updateUserName(displayName)} style={styles.button}>
-                <Text style={styles.text}>Update DisplayName</Text>
-              </Pressable>
-              <Button title="Pick an image from camera roll" onPress={pickImage} />
-              <Pressable onPress={() => updateUserPicture(image)} style={styles.button}>
-                <Text style={styles.text}>Update Picture</Text>
-              </Pressable>
-            </View>
-          )}
-          <Link href="/signin" style={styles.redirectLink} onPress={onLogout}>Logout</Link>
+          <Text style={styles.text}>Display Name: {user.displayName}</Text>
+          <TextInput
+            value={displayName}
+            onChangeText={setDisplayName}
+            style={styles.input}
+            placeholder="Display Name"
+          />
+          <Pressable onPress={updateUserName} style={styles.button}>
+            <Text style={styles.text}>Update Display Name</Text>
+          </Pressable>
+          {image && <Image source={{ uri: image }} style={styles.image} />}
+          <Button title="Pick an image from camera roll" onPress={pickImage} />
         </View>
       ) : (
         <View style={styles.userInfoContainer}>
@@ -161,6 +104,6 @@ const Profile = () => {
       )}
     </LinearGradient>
   );
-}
+};
 
 export default Profile;
